6a61bd92b9ee1d6e8d590b7c55ea6e235980aa66
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2024-06-04 17:58:20 +0100

    Added javaScript for random answer Generator

--- script.js
+++ script.js
@@ -10,11 +10,21 @@ function randomQuestion(){
     let question = document.getElementById("question");
     question.innerHTML = "What famous Irish band was phil Lynott in?" //Question object, array of question objects??
 
-    let randomAnswers = ["Thin lizzy", "Def Leopard", "The Dubliners", "U2"];
+    let randomAnswers = ["Thin lizzy", "Def Leopard", "The Dubliners", "U2", "Fish", "Cow"];
     let answersContainer = document.getElementsByClassName("answer");
+    let randAnswers = [];
 
     for (let i = 0; i < answersContainer.length; i++) {
-        answersContainer[i].innerHTML = `<li>${randomAnswers[i]}</li>`;
+        let randInt = Math.floor((Math.random() * randomAnswers.length));
+        let answer = randomAnswers[randInt];
+
+        if (randAnswers.includes(answer)){
+            i--;
+            continue;
+        }else{
+            answersContainer[i].innerHTML = `<li>${answer}</li>`;
+            randAnswers.push(answer);
+        }
     }
 }
 
