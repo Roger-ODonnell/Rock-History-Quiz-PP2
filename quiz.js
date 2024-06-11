6064b61ab01067186e38ee4a9903f6fcb20e51d1
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2024-06-12 00:10:31 +0100

    Update quiz.js - restart link update

--- quiz.js
+++ quiz.js
@@ -123,7 +123,7 @@ function inCorrect() {
 }
 
 function restart() {
-  location.href = "/index.html";
+  location.href = "https://roger-odonnell.github.io/Rock-History-Quiz-PP2/";
 }
 
 //Game over Page code
